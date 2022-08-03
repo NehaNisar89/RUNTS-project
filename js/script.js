1f5aadd65aa2c2601d7f14a185edab252c8f6483
NehaNisar89 <neha.nisar89@gmail.com>
2022-08-03 11:42:41 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: false,
+        centerMode: true,
+        centerPadding:'5px',
+        autoplaySpeed: 2000
+    });
+  });
